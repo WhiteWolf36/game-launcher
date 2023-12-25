ec09a98702b05b871f44c5de841ee59fdc8379a4
WhiteWolf36 <ahmadking78654@gmail.com>
2023-12-26 02:10:16 +0500

    Customized the theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,21 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#595959",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b3b3b3",
+      400: "#A0A0A0",
+      500: "#898989",
+      700: "#202020",
+      800: "#121212",
+      90: "#111",
+    },
+  },
+});
 
 export default theme;
