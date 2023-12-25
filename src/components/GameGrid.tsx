14e3764f1c6f7367b586a9308d3ba23f38659f9c
WhiteWolf36 <ahmadking78654@gmail.com>
2023-12-26 02:14:11 +0500

    Refactored Some Code

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,10 +15,10 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) <Text> {error}</Text>;
+
   return (
     <>
-      {error && <Text> {error}</Text>}
-
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={6} padding={10}>
         {isLoading &&
           skeletons.map((skeleton) => (
